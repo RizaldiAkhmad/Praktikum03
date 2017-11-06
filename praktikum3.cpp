5d0b4acf03f6612f96a259398c7a84ee15f2c3b2
RizaldiAkhmad <32384084+RizaldiAkhmad@users.noreply.github.com>
2017-11-06 21:55:55 +0800

    percabangan gaji bersih minggu pertama

--- praktikum3.cpp
+++ praktikum3.cpp
@@ -5,4 +5,12 @@ int main(){
 	
 	int gajiPerjam = 15000, gajiPerminggu =850000;	
 	int waktu[3],gajiLembur,gajiAwal,gajiTotal[4];
+	cout << "jam kerja minggu pertama = "; cin>> waktu[0];
+	if (waktu[0]>40){
+			gajiLembur=(waktu[0]-40)*1.5*gajiPerjam;
+			gajiTotal[0]=(gajiPerminggu+gajiLembur)*0.85;
+			gajiAwal=gajiPerminggu*0.85;
+		}else{
+			gajiTotal[0]=gajiPerminggu*0.85;
+		}
 }
