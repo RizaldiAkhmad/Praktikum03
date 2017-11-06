777ce59cc1b6ab0a6bc9268a592620690d007964
RizaldiAkhmad <32384084+RizaldiAkhmad@users.noreply.github.com>
2017-11-06 21:03:03 +0800

    Menginput dan menampilkan nilai persegi panjang

--- praktikum2.cpp
+++ praktikum2.cpp
@@ -6,4 +6,11 @@ int main (){
 	int luas,lebar,alas,tinggi;
 	char bidang;
 	cout << "jenis bidang (P atau S) ? ";cin>>bidang;
+	
+	if (bidang=='P'){
+		cout << "Nilai Tinggi = "; cin>>tinggi;
+		cout << "Nilai Lebar  = "; cin>>lebar;
+		luas = tinggi*lebar;
+		cout << "Luas Persegi Panjang Adalah" <<luas;
+	}
 }
