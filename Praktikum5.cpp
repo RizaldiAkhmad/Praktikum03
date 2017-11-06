2c9e752f467a3d81155011c208c78fdebc0054f4
RizaldiAkhmad <32384084+RizaldiAkhmad@users.noreply.github.com>
2017-11-06 23:20:03 +0800

    Percabangan else if untuk menentukan apakah seseorang itu underweight/normal/overweight

--- Praktikum5.cpp
+++ Praktikum5.cpp
@@ -6,6 +6,12 @@ int main (){
  	cout << "===== PROGRAM TES BADAN =====" << endl;
  	cout << " Tinggi Badan = "; cin >> tinggi;
  	cout << " Berat Badan = "; cin >> berat;
-   
+   		if (berat<tinggi/2.5){
+    		cout << "Underweight";
+    	}else if(tinggi/2.5  <= berat && berat <= tinggi/2.3){
+			cout << "Normal";
+		}else if(tinggi/2.3 < berat){
+			cout << "Overweight";
+	}
 
 }
